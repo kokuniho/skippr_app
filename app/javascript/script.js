d447e88d9c4a45a8e20799afb1226c9a9b60dc1c
kokuniho <ishikuni.9240@gmail.com>
2022-04-12 09:00:42 +0000

    initial commit

---
+++ app/javascript/script.js
@@ -0,0 +1,29 @@
+let title = "javascriptが使えました。";
+alert(title)
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
+
+$(document).ready(function () {
+  $("#theTarget").skippr({
+    transition :'slide',
+    speed: 1000,
+    easing: 'easeOutQuart',
+    // ナビゲーションの形("block" or "bubble")
+    navType: 'block',
+    // 子要素の種類('div' or 'img')
+    childrenElementType: 'div',
+    arrows: true,
+    // スライドショーの自動再生(falseで自動再生なし)
+    autoPlay: true,
+     // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlayDuration: 3000,
+     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    keyboardOnAlways: true,
+    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+    hidePrevious: false
+  });
+});
